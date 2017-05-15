8c4342680b37eba1d44e8acc19edeb299429c8cd
Moi <alexandrecadieux@MacBook-Pro-de-alexandre.local>
2017-05-15 17:03:56 -0400

    react-router installed and new pages created

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import auth from '../utils/Auth';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to Dialogue</h2>
+        </div>
+        Auth.
+        <p className="App-intro">
+          This is our future clinic management app!!!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import App from './components/App';
+import Booking from './components/pages/Booking';
+import Confirmation from './components/pages/Confirmation';
+
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+const routes = (
+<Router history={browserHistory}>
+  <Route path="/" component={App}>
+    <IndexRoute component={Booking}/>
+    <Route path="/boards/:id" component={Confirmation}/>
+  </Route>
+</Router>
 );
+
+ReactDOM.render(routes, document.getElementById('root'));
