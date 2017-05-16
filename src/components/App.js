223c90a72a62d698a37c20f5fe7f6cdb974eb91b
Moi <alexandrecadieux@MacBook-Pro-de-alexandre.local>
2017-05-15 21:17:15 -0400

    finally connected to Auth0!!!!

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
-import auth from '../utils/Auth';
+import Login from './pages/Login';
+import Booking from './pages/Booking'
+import AuthService from '../utils/AuthService'
 import './App.css';
 
+const auth = new AuthService('mDjKJ-cjHls9d694dX7VpOqFgM8573KE', 'dialogue-app.auth0.com')
+
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
+      <div>
+          {console.log(auth)}
           <h2>Welcome to Dialogue</h2>
-        </div>
-        Auth.
-        <p className="App-intro">
-          This is our future clinic management app!!!
-        </p>
+          {!auth.loggedIn()?
+          <Login auth={auth}/>:
+          <p>This user is connected</p>}
+
       </div>
-    );
+    )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Router, Route, browserHistory } from 'react-router';
 import App from './components/App';
 import Booking from './components/pages/Booking';
 import Confirmation from './components/pages/Confirmation';
+import Login from './components/pages/Login';
 
 import './index.css';
 
 const routes = (
 <Router history={browserHistory}>
   <Route path="/" component={App}>
-    <IndexRoute component={Booking}/>
-    <Route path="/boards/:id" component={Confirmation}/>
+    <Route path="/login" component={Login}/>
+    <Route path="/booking" component={Booking}/>
+    <Route path="/confirmation" component={Confirmation}/>
   </Route>
 </Router>
 );
