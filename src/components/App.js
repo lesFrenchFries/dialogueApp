e95b633e24694539827995343c54fa7128ab4e62
Moi <alexandrecadieux@MacBook-Pro-de-alexandre.local>
2017-05-17 12:42:43 -0400

    fixed some CSS on the menu

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,7 @@ class App extends Component {
 
   render() {
     let userInfo = this.props.route.auth.getUserInfo();
+    console.log(userInfo);
     let {isMenuOpen} = this.state;
     let {auth} = this.props.route;
     return (
