991432eba0c42c15230297e8a2e87c376bbfe7b1
Nicolas Hoog <nicolas.hoog@gmail.com>
2017-05-18 17:19:16 -0400

    pull commit

--- src/components/modals/Confirmation.js
+++ src/components/modals/Confirmation.js
@@ -1,11 +1,36 @@
 import React from 'react';
 
-class Confirmation extends React.Component {
+const ENTER = 13;
+class Confirmation extends React.Component {constructor(props) {
 
-  render() {
-    return (
-      <div></div>
-    );
+    super(props);
+    this.state = {
+    };
+  }
+
+  _handleClick = () => {
+
+  }
+
+  _handleTyping = (e) => {
+    if (e.keyCode===ENTER) {
+      this._handleClick();
+    }
+  }
+
+    render() {
+      return (
+        <div className="confirmation-form">
+          <h3 className="confirmation-title">Confirmation</h3>
+          <p>Your appointment is {this.props.data} at {this.props...} for a {this.props.location.query.position} specialiste</p>
+          <div className="confirmation-button">
+            <button
+             onClick= this._handleClick
+             onKeyUp= this._handleTyping>
+             Confrim</button>
+          </div>
+        </div>
+      );
   }
 
 }
