e9ee0a499bab80ce0b57e31586b731f84d9232a1
Nicolas Hoog <nicolas.hoog@gmail.com>
2017-05-17 20:27:33 -0400

    availability a little bit more avanced

--- src/components/modals/Availability.js
+++ src/components/modals/Availability.js
@@ -4,10 +4,12 @@ class Availability extends React.Component {
 
   render() {
     return (
-      <div>Availability</div>
+      <div className="availability">
+        <h3 className="availability-titlte">Please choose an availability for the {this.props.params.date}</h3>
+        <ul className="availability-liste"></ul>
+      </div>
     );
   }
-
 }
 
 export default Availability;
