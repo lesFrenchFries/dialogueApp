b22867177edf4e6b6149deb4012097c9b0793714
Alexandre Cadieux <cadieux.alex@gmail.com>
2017-05-18 17:03:20 -0400

    fixed the booking page to work with the api request, merging with master so Nico can work on the latest version (#6)
    * completed Booking page, added a DayButton component to generate each button for the 7 days of the week. each button leads to the corresponding availabilities page
    
    * fixed DayButton component to render gray button when no availabilities
    
    * api call for Availabilities working, now starting the DisplayAvailabilities component
    
    * Booking page done, DisplayAvailabilities working
    
    * ready to pull frenchfries master
    
    * fixed the booking page to work with the api request, now starting the booking/:id page

--- src/components/pages/Availability.js
+++ src/components/pages/Availability.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import api from '../../api';
-import DisplayAvailabilities from '../elements/DisplayAvailabilities'
+import DisplayAvailabilities from '../elements/DisplayAvailabilities';
 var moment = require('moment');
 
 
@@ -18,7 +18,7 @@ class Availability extends React.Component {
   }
 
   _fetchAvailabilities = () => {
-    api.requestAvailabilities()
+    api.reqAvailabilities(this.props.location.query.spec, this.props.params.date)
     .then(data =>{
       var date = +moment(this.state.date);
       var avTime = data.find(function(obj){
@@ -26,23 +26,33 @@ class Availability extends React.Component {
       })
 
       this.setState({
-        dayAvailabilities:avTime.availabilities,
+        dayAvailabilities:avTime.slots,
       })
     })
   }
 
   render() {
+    let {date}=this.state;
+    console.log(this.props);
     if(this.state.dayAvailabilities.length > 0){
       return (
           <div className="availability">
-            <h3 className="availability-titlte">Please choose an availability for the {this.props.params.date}</h3>
-            <ul className="availability-liste"></ul>
+            <h3 className="availability-titlte">Please choose an availability for the {date}</h3>
+            <ul className="timeSlotList">
+              {this.state.dayAvailabilities.map(timeSlot =>
+                  <DisplayAvailabilities
+                    key={timeSlot.start}
+                    data={timeSlot}
+                    auth={this.props.route.auth}
+                  />
+                )
+              }
+            </ul>
           </div>
         );
     }else{
       return <div>Loading...</div>
     }
-
   }
 }
 
---
+++ src/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  API_HOST: "https://dialogueapp-api-sebastienvuong.c9users.io"
+}
