073a7c47025a666ebaeffcc4ea4b452e9e749949
Alexandre Cadieux <cadieux.alex@gmail.com>
2017-05-19 13:33:46 -0400

    change date format in Details render

--- src/components/pages/Details.js
+++ src/components/pages/Details.js
@@ -27,17 +27,25 @@ class Details extends React.Component {
 
   render() {
     let {info} = this.state;
-    return (
-      <div className="confirmation">
-        <h2 className="confirmation-title">Your appointment is confirm for {moment(info.time).format("dddd, MMMM Do")} at {moment(info.time).format("hh:mm a ")}</h2>
-        <p>
-          You will be meeting Dr {info.firstName} {info.lastName} ({info.specialization}) at this address:<br/>
-          <span>
-            - {info.address}
-          </span>
-        </p>
-      </div>
-    );
+    console.log(info);
+    if(Object.keys(info).length>0){
+      info.time = info.time.split("T").join(" ").split(":", 2).join(":").split(" ");
+      return (
+        <div className="confirmation">
+          <h3 className="confirmation-title">Your appointment is confirm</h3>
+          <h2>{moment(info.time[0]).format("dddd, MMMM Do")} at {info.time[1]}</h2>
+          <p>
+            You will be meeting Dr {info.firstName} {info.lastName} ({info.specialization}) at this address:<br/>
+            <span>
+              - {info.address}
+            </span>
+          </p>
+        </div>
+      );
+    }else{
+      return (<div>Loading...</div>)
+    }
+
   }
 
 }
